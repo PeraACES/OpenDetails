42848e678d7e120939dc13b7284ea447bbd9ef9d
Nirmal Lankathilaka <nir3al@gmail.com>
2015-09-01 03:55:43 +0530

    Add stylesheet

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var App=angular.module('App',[])
 App.controller('mainController', function($scope, $http){
 
 
-    var hide='display:none'
-    var show=''
+    var hide='display:block'
+    var show='display:block';// NONE, not 'block'
 
     $scope.disp=hide
     $scope.msg=''
     $scope.col='green'
+    $scope.dispdetails=hide
 
     $scope.get = function () {
         $http.get('?num='+$scope.num).then(function(response) {
@@ -18,15 +19,18 @@ App.controller('mainController', function($scope, $http){
     		$scope.company=response.data.company
 	    	$scope.batch=response.data.batch
 	    	$scope.email=response.data.email
+            $scope.dispdetails=show
             if (response.data.name==undefined) {
                 $scope.msg='Sorry, we don\'t have you in our database'
                 $scope.disp=show
                 $scope.col='orange'
+                $scope.dispdetails=hide
             }
         },function(res){
             $scope.msg='Error '+res.status+' '+res.statusText
             $scope.disp=show
             $scope.col='red'
+            $scope.dispdetails=hide
         })
     }
 
@@ -47,6 +51,7 @@ App.controller('mainController', function($scope, $http){
     $scope.clearmsg = function () {
          $scope.msg=''
          $scope.disp=hide
+         $scope.dispdetails=hide
     }
 
 });
